4ee28d419e0808b66d21cf73fb399fb3bbc840d1
DzmitryMiankou <gmiankou@gmail.com>
2023-12-04 15:56:55 +0300

    EFD

--- app/src/main/java/com/example/myapplication/Exponentiation.java
+++ app/src/main/java/com/example/myapplication/Exponentiation.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
 public class Exponentiation {
-    private final long OPERATION, BASE;
+    private final long EXPONENT, BASE;
     private long value = 1;
     Exponentiation(int b, int o){
-        OPERATION = o;
+        EXPONENT = o;
         BASE = b;
     }
     protected long getResult() {
-        for(int i = 0; i < this.OPERATION; i++) this.value = this.value * this.BASE;
-        return  value;
+        for(int i = 0; i < this.EXPONENT; i++) this.value = this.value * this.BASE;
+        return (int) value;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.myapplication;
 
-        import androidx.appcompat.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView resultTextView;
-    EditText textField_1, textField_2;
-    Button addButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView resultTextView;
+        EditText textField_1, textField_2;
+        Button addButton;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 int number, number2, res;
-                String text1, text2, resString, errString, nullString ;
+                String text1, text2, resString, errString, nullString;
 
                 resString = "Результат: ";
                 errString = "Заполните все поля";
@@ -36,8 +38,9 @@ public class MainActivity extends AppCompatActivity {
                 text1 = textField_1.getText().toString();
                 text2 = textField_2.getText().toString();
 
-                if(text1.length() == 0 || text2.length() == 0) {
+                if (text1.length() == 0 || text2.length() == 0) {
                     resultTextView.setText(errString);
+                    showToast(errString);
                     return;
                 }
 
@@ -46,12 +49,19 @@ public class MainActivity extends AppCompatActivity {
                 //res = number + number2;
                 Exponentiation answer = new Exponentiation(number, number2);
 
-                if(number == 0 & number2 == 0) resultTextView.setText(nullString);
-                else {
+                if (number == 0 & number2 == 0) {
+                    resultTextView.setText(nullString);
+                    showToast(nullString);
+                } else {
                     String right_result = resString + number + " в степни " + number2 + " = " + answer.getResult();
                     resultTextView.setText(String.valueOf(right_result));
                 }
             }
         });
+    }
+
+    private void showToast(String text) {
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
